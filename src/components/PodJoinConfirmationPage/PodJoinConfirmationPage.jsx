277faf3d49acc51c961bb5c79f873da154e7f569
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-18 15:53:28 -0500

    Add text and buttons to Confirmation Page. Back button pushes back

--- src/components/PodJoinConfirmationPage/PodJoinConfirmationPage.jsx
+++ src/components/PodJoinConfirmationPage/PodJoinConfirmationPage.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './PodJoinConfirmationPage.css'
+import {useHistory} from 'react-router-dom';
 
 function PodJoinConfirmationPage() {
     const dispatch = useDispatch();
+    const history = useHistory();
+
+    const podName = useSelector(store => store.podReducer.pod_name)
+
+    const handleSubmit = () => {
+        console.log(podName);
+    }
 
     return (
         <>
-            <div>
+            <div className="body">
                 <h2>PodJoin Confirmation Page</h2>
+                <h3>You are attempting to join:</h3> 
+                    <h2>{podName}</h2>
+                <h3>If this is correct, hit 'JOIN POD'.</h3>
+                <h3>If this is incorrect, hit the 'BACK' button and enter your Key Code again.</h3>
+                <button onClick={handleSubmit}>JOIN POD</button>
+                <button onClick={() => {history.push('/podquestionpage');}}>BACK</button>
             </div>
         </>
     )
