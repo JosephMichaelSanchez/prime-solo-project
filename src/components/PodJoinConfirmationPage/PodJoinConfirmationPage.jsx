f97b21445cf32b1c129e0e2320d07d6294e50931
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-19 10:28:59 -0500

    Add selector for user to PJC, saga hears dispatch

--- src/components/PodJoinConfirmationPage/PodJoinConfirmationPage.jsx
+++ src/components/PodJoinConfirmationPage/PodJoinConfirmationPage.jsx
@@ -9,14 +9,19 @@ function PodJoinConfirmationPage() {
 
     const podName = useSelector(store => store.podReducer.pod_name)
     const podId = useSelector(store => store.podReducer.id)
+    const userId = useSelector(store => store.user.id)
 
     const handleSubmit = () => {
         console.log(podName);
-        console.log(podId);
+        console.log(podId)
+        console.log(userId);
 
         dispatch({
             type: 'JOIN_POD',
-            payload: podId
+            payload: {
+                userId: userId, 
+                podId: podId
+            }
         })
     }
 
