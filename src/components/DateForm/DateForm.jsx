faf0345a451a568cd67a932fe7ba6f0474a994b0
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-20 15:19:54 -0500

    Add input to Date Form

--- src/components/DateForm/DateForm.jsx
+++ src/components/DateForm/DateForm.jsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function DateForm() {
-
+    const [newDate, setNewDate] = useState('');
 
 
     return (
         <>
-        <h3>THIS IS THE DATE FORM</h3>
+            <div className="body">
+                <h3>THIS IS THE DATE FORM</h3>
+                <form>
+                    <div>
+                        <label htmlFor="newdate">
+                            Add New Date:
+                            <input
+                                type="text"
+                                name="newdate"
+                                value={newDate}
+                                required
+                                placeholder='YYYY/MM/DD'
+                                onChange={(event) => setNewDate(event.target.value)}
+                            />
+                        </label>
+                    </div>
+
+                </form>
+            </div>
         </>
     )
 }
