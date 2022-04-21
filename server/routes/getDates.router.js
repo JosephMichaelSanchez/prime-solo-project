30b73177c699727c456e9e80dd6fb20b5f55b699
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-20 20:33:07 -0500

    Create getDates router

---
+++ server/routes/getDates.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+    rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+router.get('/:pod', rejectUnauthenticated, (req, res) => {
+    console.log(req.params.pod);
+
+    const query = `SELECT * FROM hosting WHERE "pod_id" = $1`;
+    const values = [req.params.pod]
+    pool.query(query, values)
+        .then(result => {
+            res.send(result.rows);
+            console.log(result);
+
+        })
+        .catch(err => {
+            console.log('ERROR GETTING POD DATES', err);
+            res.sendStatus(500)
+        })
+
+});
+
+module.exports = router;
