ac2dffa676451aee7323ecc4fd02124ad4ef62ba
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-21 22:16:12 -0500

    Add conditional render for Admin DELETE date button

--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -5,6 +5,7 @@ function TableRow({ date }) {
 
     const dispatch = useDispatch();
     const user = useSelector(store => store.user);
+    const podInfo = useSelector(store => store.podInfoReducer);
 
     const handleDelete = () => {
         console.log('the date id is', date.id);
@@ -39,7 +40,7 @@ function TableRow({ date }) {
             <tr>
                 <td>{date.date}</td>
                 <td>{date.host == 'NEEDS HOST' ? <button onClick={handleAddHost}>HOST</button> : <p>{date.host}</p>}</td>
-                <td><button onClick={handleDelete}>DELETE</button></td>
+                <td>{user.id == podInfo.admin_id ? <button onClick={handleDelete}>DELETE</button> : <p></p>}</td>
                 <td>{date.host == user.last_name && <button onClick={handleCancel}>CANCEL</button>}</td>
             </tr>
         </>
