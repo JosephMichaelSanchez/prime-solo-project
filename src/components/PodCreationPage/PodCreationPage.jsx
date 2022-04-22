82d2364ebb40ef4323be9801968b061db62325b8
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-22 10:37:52 -0500

    Create a form for creating a new pod and key code

--- src/components/PodCreationPage/PodCreationPage.jsx
+++ src/components/PodCreationPage/PodCreationPage.jsx
@@ -4,10 +4,53 @@ import { useHistory } from 'react-router-dom';
 
 function PodCreationPage() {
 
+    const [newPodName, setNewPodName] = useState('')
+    const [keyCode, setKeyCode] = useState('')
+
+    const handleSubmit = () => {
+        console.log('clicked');
+    }
+
     return (
         <>
             <div className="body">
                 <h2>THIS IS THE POD CREATION PAGE</h2>
+                <form>
+                    <div>
+                        <label htmlFor="newdate">
+                            Create a name for your new pod:
+                            <input
+                                type="text"
+                                name="newpod"
+                                value={newPodName}
+                                placeholder='POD NAME'
+                                onChange={(event) => setNewPodName(event.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="newpodkeycode">
+                            Create a Key Code for your pod:
+                            <input
+                                type="text"
+                                name="newkeycode"
+                                value={keyCode}
+                                placeholder='KEY CODE'
+                                onChange={(event) => setNewPodName(event.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <h4>Your Key Code is what you will share with people you want to join your pod.</h4>
+                    </div>
+                    <div>
+                        <button onClick={handleSubmit}>CREATE NEW POD</button>
+             
+                    </div>
+
+
+                </form>
+                
             </div>
         </>
 
