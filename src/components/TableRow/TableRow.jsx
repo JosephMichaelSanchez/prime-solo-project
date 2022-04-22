2a682b7ab55d36e60b03ac5a25722dbe58565273
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-21 19:56:19 -0500

    Add conditional rendering to table rows

--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 function TableRow({ date }) {
 
     const dispatch = useDispatch();
+    const user = useSelector(store => store.user);
 
     const handleDelete = () => {
         console.log('the date id is', date.id);
@@ -16,19 +17,22 @@ function TableRow({ date }) {
 
     }
 
-    const handleRender = () => {
-        if(date.host == 'NEEDS HOST') {
-            return <button>NEEDS HOST</button>
+    // const handleRender = () => {
+    //     if(date.host == 'NEEDS HOST') {
+    //         return <button>NEEDS HOST</button>
 
-        }
-    }
+    //     } else if(date.host == user.last_name) {
+    //         return <button>CANCEL HOSTING</button>
+    //     } 
+    // }
 
     return (
         <>
             <tr>
                 <td>{date.date}</td>
-                <td>{handleRender()}</td>
+                <td>{date.host == 'NEEDS HOST' ? <button>HOST</button> : <p>{date.host}</p>}</td>
                 <td><button onClick={handleDelete}>DELETE</button></td>
+                <td>{date.host == user.last_name && <button>CANCEL</button>}</td>
             </tr>
         </>
     )
